b19083cb1bc3ad7595bfe231c5fc5e9615c52bdc
AndroidNetrunner <kby9906@naver.com>
2022-10-31 21:39:57 +0900

    [ADD]Game reducer 추가 및 redux 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
-import 'bootstrap/dist/css/bootstrap.css'
+import type { AppProps } from "next/app";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import GlobalStyle from "../components/globalstyles";
+import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from "react-redux";
+import { wrapper } from "../store/index.js";
 
 const theme: DefaultTheme = {
   colors: {
-    primary: '#111',
-    secondary: '#0070f3',
+    primary: "#111",
+    secondary: "#0070f3",
   },
-}
+};
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -18,5 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
+
+export default wrapper.withRedux(App);
