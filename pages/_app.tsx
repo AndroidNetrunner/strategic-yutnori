338d17c06b03ba03300977d7ab4e3011e28d6181
김병윤 <byung-yoon@gimbyeong-yun-ui-MacBookPro.local>
2022-11-04 21:33:55 +0900

    [ADD] game.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider, DefaultTheme } from 'styled-components'
 import GlobalStyle from '../components/globalstyles'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import { Provider } from 'react-redux'
 const theme: DefaultTheme = {
   colors: {
     primary: '#111',
@@ -12,11 +12,11 @@ const theme: DefaultTheme = {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </Provider>
   )
 }
