2402350802c40efc1772bed15bdce54d30a58da5
김병윤 <byung-yoon@gimbyeong-yun-ui-MacBookPro.local>
2022-11-07 09:43:40 +0900

    [FIX] add break on 개,걸,윷

--- components/ThrownYuts.tsx
+++ components/ThrownYuts.tsx
@@ -1,21 +1,24 @@
-export default function ThrownYuts({myYut, opponentYut}) {
-    let result;
-    switch (myYut + opponentYut) {
-        case 0:
-            result = "모";
-        break;
-        case 1:
-            result = "뒷도";
-        break;
-        case 2:
-            result = "개";
-        case 3:
-            result = "걸";
-        case 4:
-            result = "윷";
-
-    }
-    return <>
-    <div>던져진 윷: {result}</div>
+export default function ThrownYuts({ myYut, opponentYut }) {
+  let result;
+  switch (myYut + opponentYut) {
+    case 0:
+      result = "모";
+      break;
+    case 1:
+      result = "뒷도";
+      break;
+    case 2:
+      result = "개";
+      break;
+    case 3:
+      result = "걸";
+      break;
+    case 4:
+      result = "윷";
+  }
+  return (
+    <>
+      <div>던져진 윷: {result}</div>
     </>
+  );
 }
